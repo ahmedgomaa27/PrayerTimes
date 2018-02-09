6810e05cd9c53434347d5c112bcce7242d6abac8
ahmedgomaa <ahmed.gomaa2703@gmail.com>
2018-02-09 20:08:45 +0200

    add title for qibla activity

--- app/src/main/java/com/example/ahmedhamdy/prayertimes/QiblaActivity.java
+++ app/src/main/java/com/example/ahmedhamdy/prayertimes/QiblaActivity.java
@@ -3,6 +3,7 @@ package com.example.ahmedhamdy.prayertimes;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class QiblaActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class QiblaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qibla);
+
+        Toolbar mToolbar =  findViewById(R.id.action_bar);
+        mToolbar.setTitle(R.string.qibla_activty_title);
+        setSupportActionBar(mToolbar);
+
 
         if (savedInstanceState == null){
 
